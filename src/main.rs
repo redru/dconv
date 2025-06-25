4cfc7e953e4179d3a3d8604a3ae293bece076397
Luca Zenobi <luca.zenobi@sysdig.com>
2025-06-25 10:00:45 +0200

    Print also in local TZ

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fmt::Debug;
 
 use anyhow::{Context, Result, bail};
-use chrono::DateTime;
+use chrono::{DateTime, Local};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -13,24 +13,47 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
+    let result = convert(&args.date)?;
+    println!("{}", result);
 
-    if let Ok(timestamp) = args.date.parse::<i64>() {
-        let date_time = match args.date.len() {
+    Ok(())
+}
+
+fn convert(input: &str) -> Result<String> {
+    if let Ok(timestamp) = input.parse::<i64>() {
+        let date_time = match input.len() {
             10 => DateTime::from_timestamp(timestamp, 0),
             13 => DateTime::from_timestamp_millis(timestamp),
             16 => DateTime::from_timestamp_micros(timestamp),
             19 => Some(DateTime::from_timestamp_nanos(timestamp)),
-            _ => bail!("Timestamp of size {} is not supported", args.date.len()),
+            _ => bail!("Timestamp of size {} is not supported", input.len()),
         }
         .context("Failed to parse timestamp")?;
 
-        println!("{}", date_time.to_rfc3339());
-        Ok(())
+        Ok(format!(
+            "{}\n{}",
+            date_time,
+            date_time.with_timezone(Local::now().offset())
+        ))
     } else {
         let date_time =
-            DateTime::parse_from_rfc3339(&args.date).context("Error parsing rfc3339 string")?;
+            DateTime::parse_from_rfc3339(input).context("Error parsing rfc3339 string")?;
 
-        println!("{}", date_time.timestamp_millis());
-        Ok(())
+        Ok(date_time.timestamp_millis().to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn test_convert_rfc3339() {
+        assert_eq!(
+            convert(&"2025-06-24T20:18:00Z").expect("Failed to convert str"),
+            String::from_str("1750796280000").expect("Failed to parse str")
+        );
     }
 }
